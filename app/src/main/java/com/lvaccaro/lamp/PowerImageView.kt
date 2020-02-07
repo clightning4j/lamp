9db400f59ae4c0c5b40665854793988007f76146
Luca Vaccaro <luck87@gmail.com>
2020-02-07 08:41:00 +0100

    Migrate PowerImageView to AppCompatImageView parent class

--- app/src/main/java/com/lvaccaro/lamp/PowerImageView.kt
+++ app/src/main/java/com/lvaccaro/lamp/PowerImageView.kt
@@ -3,16 +3,15 @@ package com.lvaccaro.lamp
 import android.content.Context
 import android.graphics.drawable.AnimationDrawable
 import android.util.AttributeSet
-import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
 
-class PowerImageView: ImageView {
+class PowerImageView: AppCompatImageView {
 
     private var light = false
 
     constructor(context: Context?): super(context)
     constructor(context: Context?, attrs: AttributeSet?): super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
 
     fun on() {
         light = true
