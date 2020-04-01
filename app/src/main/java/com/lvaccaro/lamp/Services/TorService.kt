1f6db87f91101e3680ff430fc9e6bb08e90ca3ab
Luca Vaccaro <luck87@gmail.com>
2020-04-01 21:11:35 +0200

    Clear tor logfile before starting tor

--- app/src/main/java/com/lvaccaro/lamp/Services/TorService.kt
+++ app/src/main/java/com/lvaccaro/lamp/Services/TorService.kt
@@ -54,6 +54,8 @@ class TorService : IntentService("TorService") {
         pb.directory(binaryDir)
         pb.redirectErrorStream(true)
         val logFile = File(rootDir(), "$daemon.log")
+        logFile.delete()
+        logFile.createNewFile()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             pb.redirectOutput(ProcessBuilder.Redirect.appendTo(logFile))
         }
