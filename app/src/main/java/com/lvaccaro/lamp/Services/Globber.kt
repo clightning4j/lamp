76608b0ef2dbe7680666e7acf9c40d08d1a94bf1
Luca Vaccaro <luck87@gmail.com>
2020-04-19 22:06:44 +0200

    Write Gobbler logs in output

--- app/src/main/java/com/lvaccaro/lamp/Services/Globber.kt
+++ app/src/main/java/com/lvaccaro/lamp/Services/Globber.kt
@@ -1,15 +1,20 @@
 package com.lvaccaro.lamp.Services
 
+import android.util.Log
 import java.io.*
+import java.util.logging.Logger
 
 class Globber(val stream: InputStream, val file: File): Thread() {
+    val log = Logger.getLogger(LightningService::class.java.name)
+
     override fun run() {
         try {
             val isr = InputStreamReader(stream)
             val br = BufferedReader(isr)
             while(!Thread.currentThread().isInterrupted()) {
                 val line = br.readLine()
-                file.appendText(line)
+                file.appendText(line ?: "")
+                log.info(line)
             }
         } catch (ioe: IOException) {
             ioe.printStackTrace()
