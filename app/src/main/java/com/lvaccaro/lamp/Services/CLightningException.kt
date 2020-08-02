c8ce0fa81b360211181d9430493d1aaf9b7063df
Luca Vaccaro <me@lvaccaro.com>
2020-08-02 12:47:38 +0200

    Migrate CLightning exception class to kotlin

---
+++ app/src/main/java/com/lvaccaro/lamp/Services/CLightningException.kt
@@ -0,0 +1,15 @@
+package com.lvaccaro.lamp.Services
+
+class CLightningException : Exception {
+    constructor() : super() {}
+    constructor(message: String?) : super(message) {}
+    constructor(message: String?, cause: Throwable?) : super(message, cause) {}
+    constructor(cause: Throwable?) : super(cause) {}
+    protected constructor(
+        message: String?,
+        cause: Throwable?,
+        enableSuppression: Boolean,
+        writableStackTrace: Boolean
+    ) : super(message, cause, enableSuppression, writableStackTrace) {
+    }
+}
