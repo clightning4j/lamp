b212ef41a4f01737fdecd7cdfcf1ef6f6f48f005
Luca Vaccaro <me@lvaccaro.com>
2020-11-06 14:35:38 +0100

    Add notification for new onchain tx

---
+++ app/src/main/java/com/lvaccaro/lamp/handlers/NewTransaction.kt
@@ -0,0 +1,29 @@
+package com.lvaccaro.lamp.handlers
+
+import android.content.Context
+import android.content.Intent
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.lvaccaro.lamp.utils.UI
+
+class NewTransaction: IEventHandler {
+
+    companion object{
+        val TAG = NewChannelPayment::class.java.canonicalName
+        val NOTIFICATION: String = "NODE_NOTIFICATION_NEW_TRANSACTION"
+        val PATTERN = "wallet: Owning output"
+    }
+
+    override fun doReceive(context: Context, information: String) {
+        if(information.contains(PATTERN)){
+            val regex = "wallet: Owning output \\d [\\w|[^:]]+".toRegex()
+            val amount = regex.find(information)?.value?.split(" ")?.get(4)
+            val intent = Intent()
+            intent.action = NOTIFICATION
+            intent.putExtra("message", "new transaction")
+            intent.putExtra("amount", amount)
+            LocalBroadcastManager.getInstance(context).sendBroadcast(intent)
+            UI.notification(context, "New onchain transaction", "${amount}")
+            UI.showMessageOnToast(context,"New onchain transaction ${amount}")
+        }
+    }
+}
